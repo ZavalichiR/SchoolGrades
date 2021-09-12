5e4c793d4022d27c8da4368ffd61e53e9d0510a7
Alaswek <casianm8@yahoo.com>
2021-09-13 00:00:29 +0300

    Updates 12.09
    -> The application corners have been removed.
    -> The application can be moved to the desktop.
    -> The function for Close has been changed
    -> The workspace in MainWindow.xaml has been changed to LoginWindow.xaml.

---
+++ SchoolGrades/Views/LoginWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+namespace SchoolGrades.Views
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Application ShutDown
+        private void Close( object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        // Movable Window
+        private void Move_Window(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if(e.LeftButton == System.Windows.Input.MouseButtonState.Pressed )
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
