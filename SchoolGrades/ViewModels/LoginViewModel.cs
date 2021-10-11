fb1bce433f33a8baf05b3fa37dccefc6679214c8
Alaswek <casianm8@yahoo.com>
2021-10-11 21:44:57 +0300

    Update LoginViewModel
    .....

--- SchoolGrades/ViewModels/LoginViewModel.cs
+++ SchoolGrades/ViewModels/LoginViewModel.cs
@@ -9,74 +9,16 @@ namespace SchoolGrades.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
-        // LoginPasswordViewModel
-        #region Attributes
-
-        private bool _loginFailed;
-        public bool LoginFailed
-        {
-            get => _loginFailed;
-            set
-            {
-                _loginFailed = value;
-                OnPropertyChanged("Login Failed");
-            }
-        }
-
-        private string _errorMessage;
-        public string ErrorMessage
-        {
-            get => _errorMessage;
-            set
-            {
-                if (value != _errorMessage)
-                {
-                    _errorMessage = value;
-                    OnPropertyChanged("Error Message");
-                }
-            }
-        }
-
-        private string _userName;
-        public string UserName
-        {
-            get => _userName;
-            set
-            {
-                if (!string.Equals(value.ToString(), _userName, StringComparison.OrdinalIgnoreCase))
-                {
-                    _userName = value;
-                    OnPropertyChanged("Username");
-                }
-            }
-        }
-
-        private SecureString _password;
-        public SecureString PasswordSecureString
-        {
-            get => _password;
-            set
-            {
-                if (value != null)
-                {
-                    _password = value;
-                    OnPropertyChanged("Passowrd");
-                }
-            }
-        }
-
-        #endregion
-
         #region Commands
-
         public ICommand LoginCommand { get; }
-
+        public ICommand ResetPasswordCommand { get; }
 
         #endregion
 
         public LoginViewModel()
         {
             LoginCommand = new RelayCommand(_ => Login());
+            ResetPasswordCommand = new RelayCommand(_ => Reset());
         }
 
         #region Private Methods
@@ -87,26 +29,9 @@ namespace SchoolGrades.ViewModels
         }
         #endregion
 
-
-        #region PasswordViewModel
-        public class ResetPasswordViewModel : ViewModelBase
+        private void Reset()
         {
-            #region Command
-            public ICommand ResetPasswordCommand { get; }
-            #endregion
-
-            public ResetPasswordViewModel()
-            {
-                ResetPasswordCommand = new RelayCommand(_ => ResetPassword());
-            }
-
-            #region Private Methods
-            private void ResetPassword()
-            {
-                throw new NotImplementedException();
-            }
-            #endregion
-            #endregion
+            throw new NotImplementedException();
         }
     }
 }
